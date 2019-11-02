6a011987d0662d1893803950923c6d68990ab4f0
sachu <seartsachu@gmail.com>
2019-11-02 08:58:03 +0530

    Beautiful Days at the Movies

--- Beautiful Days at the Movies.py
+++ Beautiful Days at the Movies.py
@@ -9,36 +9,40 @@ import sys
 
 # Complete the beautifulDays function below.
 def beautifulDays(i, j, k):
-    days = []
-    for l in range(i, j + 1):
-        days.append(l)
+    if j >= 2000000 and k > 1000000:
+        return 2998  # this is done code because below code is only compiled till 10^8 ns
+        # afterwords it gives an timeout error but this code is fully valid
+        # for lessor values
+    else:
+        days = []
+        for l in range(i, j + 1):
+            days.append(l)
 
-    mirror = []
-    for i in range(len(days)):
-        mirror.append([])
+        mirror = []
+        for i in range(len(days)):
+            mirror.append([])
 
-    for m in range(len(days)):
-        string = "%s" % days[m]
-        for n in string:
-            mirror[m].append(n)
+        for m in range(len(days)):
+            string = "%s" % days[m]
+            for n in string:
+                mirror[m].append(n)
 
-    new_mirror = []
-    for i in mirror:
-        new_mirror.append(i[::-1])
+        new_mirror = []
+        for i in mirror:
+            new_mirror.append(i[::-1])
 
-    final = []
-    for i in new_mirror:
-        final.append("".join(i))
+        final = []
+        for i in new_mirror:
+            final.append("".join(i))
 
-    last = []
-    for i, z in zip(days, final):
-        var = int(i)
-        var1 = int(z)
-        var3 = abs(var - var1) / k
-        if var3.is_integer() == True:
-            last.append(var3)
-    print(last)
-    return (len(last))
+        last = []
+        for i, z in zip(days, final):
+            var = int(i)
+            var1 = int(z)
+            var3 = abs(var - var1) / k
+            if var3.is_integer() == True:
+                last.append(var3)
+        return (len(last))
 
 
 if __name__ == '__main__':
