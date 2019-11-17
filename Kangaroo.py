9a7d7c87dfd6a1de3d79df1172d96829c24b1667
sachu <seartsachu@gmail.com>
2019-11-17 22:53:57 +0530

    Kangaroo

--- Kangaroo.py
+++ Kangaroo.py
@@ -21,13 +21,13 @@ def kangaroo(x1, v1, x2, v2):
     else:
         return("NO")
 
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     x1V1X2V2 = input().split()
 
     x1 = int(x1V1X2V2[0])
-
 
     v1 = int(x1V1X2V2[1])
 
