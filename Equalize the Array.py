3f1cbe7c536b4f69e1c99903607e1d1b56f8398d
sachu <seartsachu@gmail.com>
2019-11-17 22:56:41 +0530

    Equalize the array

--- Equalize the Array.py
+++ Equalize the Array.py
@@ -12,17 +12,17 @@ def equalizeArray(arr):
     n_arr = list(set(arr))
     # firstly we make unique elements of the array in order to count the number of occurence in the given array
 
-    count = [] # in this array we insert , a how many times a element of the array would oocur
+    count = []  # in this array we insert , a how many times a element of the array would oocur
 
     for i in n_arr:
-        var = [i,arr.count(i)]
+        var = [i, arr.count(i)]
         count.append(var)
     # count[0] will give the number
     # count[1] will give the occurence of a number in a given array
 
     # we have to make equal array so, we find the number who occur max time
     # remove that number from the array
-    count.remove(max(count,key=lambda x: x[1]))
+    count.remove(max(count, key=lambda x: x[1]))
     # so , we sum up all oocurence of a element of remaining elements of the array
     # i.e., our answer
     counter = 0
